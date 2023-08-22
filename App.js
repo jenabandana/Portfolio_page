c33e62e878786c5f902e85aea7db0368076bc06c
Bandana Jena <141347968+jenabandana@users.noreply.github.com>
2023-08-22 20:56:47 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Routes , Route } from 'react-router-dom'
+import Home from './routes/Home'
+import About from './routes/About'
+import Contact from './routes/Contact'
+import Project from './routes/Project'
+
+const App = () => {
+  return (
+    <>
+    <Routes>
+      <Route path ='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/contact' element={<Contact />} />
+      <Route path='/project' element={<Project />} />
+    </Routes>
+    </>
+  )
+}
+
+export default App
